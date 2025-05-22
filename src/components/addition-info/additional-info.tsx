a45b68aad5e3c1c0c4766c0bc0ce3cb5c54a3dae
JanKubesIsBest <jan.kubes07@gmail.com>
2025-05-22 12:42:16 +0200

    fix additional info

--- src/components/addition-info/additional-info.tsx
+++ src/components/addition-info/additional-info.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { Recipe } from "@/utils/together-api/recipe-utils";
 import SearchComponent from "../search/search-component";
 import TogetherAPI from "@/utils/together-api/recipe-utils";
 import styles from "./AdditionalInfo.module.css";
+import { Recipe } from "../../utils/together-api/interfaces";
+import { askFollowUpQuestion } from "@/utils/together-api/actions";
 
 interface AdditionalInfoProps {
   info: string | undefined;
@@ -12,12 +13,11 @@ interface AdditionalInfoProps {
 
 export default function AdditionalInfo({ info, recipe, discard }: AdditionalInfoProps) {
   const [followUpResponse, setFollowUpResponse] = useState<string | null>(null);
-  const together = new TogetherAPI();
 
   const handleSearch = async (query: string) => {
     if (info) {
       try {
-        const response = await together.askFollowUpQuestion(query, recipe, info);
+        const response = await askFollowUpQuestion(query, recipe, info);
         setFollowUpResponse(response);
       } catch (err) {
         console.error("Failed to fetch follow-up info:", err);
